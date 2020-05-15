946e27d25a1530bcec351f34a09232942dfd4db5
EC2 Default User <ec2-user@ip-172-31-19-183.us-east-2.compute.internal>
2020-05-15 01:51:06 +0000

    Changing market hours to hardcoded. Adding queues in template.

--- robo_trade/trade_lambda/market_hours.py
+++ robo_trade/trade_lambda/market_hours.py
@@ -4,7 +4,6 @@ __author__ = "Nathan Ward"
 import logging
 from datetime import date, datetime
 from pytz import timezone, utc
-import boto3
 
 _LOGGER = logging.getLogger()
 _LOGGER.setLevel(logging.INFO)
@@ -18,33 +17,49 @@ def get_market_open_close() -> dict:
     date_format = '%H:%M:%S'
     current_time = datetime.now(utc)
     
-    ssm_client = boto3.client('ssm')
-    try:
-        market_open = ssm_client.get_parameter(Name = 'EpithyTrader_MarketOpen')['Parameter']['Value']
-        market_close = ssm_client.get_parameter(Name = 'EpithyTrader_MarketClosed')['Parameter']['Value']
-    except Exception as e:
-        _LOGGER.error('Unable to grab marked open or close from SSM. {0}'.format(e))
-        raise Exception('Unable to grab marked open or close from SSM. {0}'.format(e))
+    #Regular hours
+    market_normal_open = '09:30:00'
+    market_normal_close = '16:00:00'
     
-    market_open_tz = timezone(market_open.rsplit(':', 1)[1])
-    market_close_tz = timezone(market_close.rsplit(':', 1)[1])
+    #Extended hours for stock
+    market_extended_open = '09:00:00'
+    market_extended_close = '18:00:00'
     
-    market_open_datetime = datetime.strptime(market_open.rsplit(':', 1)[0], date_format).time()
-    market_close_datetime = datetime.strptime(market_close.rsplit(':', 1)[0], date_format).time()
+    #New york timezone for nasdaq/nyse, same timezone as us-east-1
+    market_tz = timezone('US/Eastern')
+    
+    market_open_datetime = datetime.strptime(market_normal_open, date_format).time()
+    market_extended_open_datetime = datetime.strptime(market_extended_open, date_format).time()
+    market_close_datetime = datetime.strptime(market_normal_close, date_format).time()
+    market_extended_close_datetime = datetime.strptime(market_extended_close, date_format).time()
     
     naive_open_datetime = datetime.combine(today, market_open_datetime)
+    naive_extended_open_datetime = datetime.combine(today, market_extended_open_datetime)
     naive_close_datetime = datetime.combine(today, market_close_datetime)
+    naive_extended_close_datetime = datetime.combine(today, market_extended_close_datetime)
     
-    open_local_datetime = market_open_tz.localize(naive_open_datetime, is_dst=None)
-    close_local_datetime = market_close_tz.localize(naive_close_datetime, is_dst=None)
+    open_local_datetime = market_tz.localize(naive_open_datetime, is_dst=None)
+    open_extended_local_datetime = market_tz.localize(naive_extended_open_datetime, is_dst=None)
+    close_local_datetime = market_tz.localize(naive_close_datetime, is_dst=None)
+    close_extended_local_datetime = market_tz.localize(naive_extended_close_datetime, is_dst=None)
     
     open_utc_converted_datetime = open_local_datetime.astimezone(utc)
+    open_extended_utc_converted_datetime = open_extended_local_datetime.astimezone(utc)
     close_utc_converted_datetime = close_local_datetime.astimezone(utc)
+    close_extended_utc_converted_datetime = close_extended_local_datetime.astimezone(utc)
     
     time_to_close = current_time - close_utc_converted_datetime
+    extended_time_to_close = current_time - close_extended_utc_converted_datetime
+    time_to_open = open_utc_converted_datetime - current_time
+    extended_time_to_open = open_extended_utc_converted_datetime - current_time
     
     return {
         'market_open': open_utc_converted_datetime,
         'market_close': close_utc_converted_datetime,
-        'time_to_close': time_to_close.total_seconds()
+        'time_to_close': time_to_close.total_seconds(),
+        'time_to_open': time_to_open.total_seconds(),
+        'extended_market_open': open_extended_utc_converted_datetime,
+        'extended_market_close': close_extended_utc_converted_datetime,
+        'extended_time_to_close': extended_time_to_close.total_seconds(),
+        'extended_time_to_close': extended_time_to_open.total_seconds()
     }
