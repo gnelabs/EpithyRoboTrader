069283be6077e5c4e5a4a017c1b0aa55b33b05dc
EC2 Default User <ec2-user@ip-172-31-19-183.us-east-2.compute.internal>
2020-05-22 19:33:48 +0000

    Add api to get robinhood token expiry.

---
+++ robo_trade/api/get_remaining_rh_token_hours.py
@@ -0,0 +1,34 @@
+
+__author__ = "Nathan Ward"
+
+import os
+import json
+import logging
+from datetime import datetime, timezone
+import boto3
+from views import register_view
+from api.utils import LambdaMessageEncoder
+from trade_lambda.get_credentials import robinhood_creds
+
+_LOGGER = logging.getLogger()
+_LOGGER.setLevel(logging.INFO)
+
+@register_view('/api/get_remaining_rh_token_hours')
+def lambda_handler(event, context):
+    """
+    Route that returns the number of hours remaining on the Robinhood
+    API auth token. This tells the user whether or not they need to refresh.
+    """
+    _robinhood_credentials = robinhood_creds()
+    expiry = _robinhood_credentials['expiry']
+    time_left = expiry - datetime.now(timezone.utc)
+    hours_remaining_rounded_down = int(time_left.total_seconds() / 3600)
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps(
+            {'remaining_hours': hours_remaining_rounded_down},
+            cls=LambdaMessageEncoder
+        ),
+        'headers': {'Content-Type': 'application/json'}
+    }
